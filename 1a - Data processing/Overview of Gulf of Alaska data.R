c4148a92ba0f002f9ce17f0e56e9844940a3ef9f
alicedwoodley <alicedwoodley@gmail.com>
2026-04-19 14:40:15 +0100

    Update Overview of Gulf of Alaska data.R
    Changes to plot for project report

--- 1a - Data processing/Overview of Gulf of Alaska data.R
+++ 1a - Data processing/Overview of Gulf of Alaska data.R
@@ -3,6 +3,7 @@
 ### Packages
 library(ggplot2)
 library(ggokabeito)
+library(ggpubr)
 
 ### Load in data
 load("~/GitHub/Extended-Independent-Project/Gulf_Of_Alaska.RData")
@@ -13,24 +14,29 @@ recruits <- GoA.data[,c(1,2,4)]
 
 # SSB plot
 
-ggplot(ssb, aes(x = year, y = log(ssb), col = species, group = species)) + 
-  geom_line(linewidth = 1.5) + 
-  scale_color_okabe_ito(labels = GoA.species$commonname, name = "Species") + 
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 7)) +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 11.8)) +
-  xlab("Year") +
-  ylab("log(SSB)") +
-  ggtitle("Spawning stock biomass of 6 species in the Gulf of Alaska 1980-2017")
+p1 <- ggplot(ssb, aes(x = year, y = log(ssb), col = species, group = species)) + 
+      geom_line(linewidth = 1.5) + 
+      scale_color_okabe_ito(labels = GoA.species$commonname, name = "Species") + 
+      scale_x_continuous(breaks = scales::pretty_breaks(n = 7)) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5, size = 12)) +
+      theme(axis.title = element_text(size = 12)) +
+      xlab("Year") +
+      ylab("log(SSB [tonnes] )") +
+      ggtitle("Spawning stock biomass of 6 species in the Gulf of Alaska 1980-2017")
 
 # Recruitment plot
 
-ggplot(recruits, aes(x = year, y = log(recruits), col = species, group = species)) + 
-  geom_line(linewidth = 1.5) + 
-  scale_color_okabe_ito(labels = GoA.species$commonname, name = "Species") +
-  scale_x_continuous(breaks = scales::pretty_breaks(n = 7)) +
-  theme_bw() +
-  theme(plot.title = element_text(hjust = 0.5, size = 11.8)) +
-  xlab("Year") +
-  ylab("log(R)") +
-  ggtitle("Recruitment of 6 species in the Gulf of Alaska 1980-2017")
+p2 <- ggplot(recruits, aes(x = year, y = log(recruits), col = species, group = species)) + 
+      geom_line(linewidth = 1.5) + 
+      scale_color_okabe_ito(labels = GoA.species$commonname, name = "Species") +
+      scale_x_continuous(breaks = scales::pretty_breaks(n = 7)) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5, size = 12)) +
+      theme(axis.title = element_text(size = 12)) +
+      xlab("Year") +
+      ylab("log(R)") +
+      ggtitle("Recruitment of 6 species in the Gulf of Alaska 1980-2017")
+
+ggarrange(p1, p2, ncol = 1, nrow = 2, common.legend = T, legend = "bottom")
+
